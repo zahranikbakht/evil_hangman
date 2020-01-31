eb5b2369616ae62bc05db7a541ef4aa12ee5cf09
zahranikbakht <60522280+zahranikbakht@users.noreply.github.com>
2020-01-31 18:58:48 -0500

    Update Evil_Hangman.py

--- Evil_Hangman.py
+++ Evil_Hangman.py
@@ -1,11 +1,17 @@
+# Evil Hangman - the classic hangman with an evil touch where the chosen word changes to make the player lose more turns
+# simple GUI
 import sys
 import stdio
 import random
 import draw
 import stddraw
+
+# read a text file (list of words to be used) in the argument and put them in a list
 mlist = open(sys.argv[1])
 words = mlist.readlines()
 mlist.close()
+
+# initializing game variables
 letters = []
 wrong_letters = []
 correct_letters = []
@@ -24,6 +30,8 @@ num = 0
 correct_guess = 0
 correct_letters = []
 dublicate = False
+
+# pick a random word from the list of words
 while len(word)< 4 or len(word)>9:
 	number = random.randrange(0,len(words))
 	word = words[number].rstrip()
@@ -39,9 +47,15 @@ for i in list_of_words	:
 		letters.append(["-",i[j]])
 	list_of_letters.append(letters)
 	letters = []
+	
+# show the current status in gui
 draw.draw_hangman_dash(word)
 stddraw.show(0)
-while guesses > 0 and correct_guess != len(word):		
+
+# while the player has turns, keep playing
+while guesses > 0 and correct_guess != len(word):
+	
+	# print the number of guesses remaining
 	print("The secret word looks like:")
 	for i in range(len(word)):
 		stdio.write(secret_word[i])
@@ -51,6 +65,8 @@ while guesses > 0 and correct_guess != len(word):
 		print("You have " + str(guesses) + " guesses remaining.")
 	else:
 		print("\nYou have " + str(guesses) + " guesses remaining.")
+	
+	# input the next letter from the player
 	letter = input("What's your next guess? ")
 	for j in range(len(list_of_letters)):		
 		for i in range (len(word)):
@@ -115,9 +131,9 @@ while guesses > 0 and correct_guess != len(word):
 			list_of_letters[j][i][0] = "-" 
 	found = False
 
-		
+# check if the player has won or lost		
 if guesses > 0:
-	print("Congradulations!")
+	print("Congragulations!")
 	stdio.write("You guessed the secret word: ")
 	for i in range (len(word)):
 		stdio.write(list_of_letters[0][i][1])
